47d873992123e2f6db1d9bb2e12ee1fc7b2a43e8
Christian Chacón <chacb.dev@gmail.com>
2024-02-02 15:29:08 -0300

    tooltips y jquery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
 $(function () {
+  // jq procesos aplicados
 
     const myCarouselElement = document.querySelector('#carrusel_viajes')
     const carousel = new bootstrap.Carousel(myCarouselElement, {
@@ -9,24 +10,5 @@ $(function () {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
   
-    // jq procesos aplicados
-    /* $("#enviarCorreo").click(function () {
-      alert("El correo fue enviado correctamente...");
-    });
-    // se coloca el titulo en rojo al hacer doble click
-    $(".ingredientes>H3").dblclick(function () {
-      $(".preparacion>H3").css("background-color", "white")
-      $(".ingredientes>H3").css("background-color", "red")
-    });
   
-    $(".preparacion>H3").dblclick(function () {
-  
-      $(".ingredientes>H3").css("background-color", "white")
-      $(".preparacion>H3").css("background-color", "red")
-    });
-  
-    // se ocultan los p de las tarjetas
-    $(".tarjetas").click(function () {
-      $(".tarjetas+p").toggle(2000)
-    }); */
   });
